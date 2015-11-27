ceb611cf99be4ffcbe2774cf18220f04622bbf09
James Fish <james@fishcakez.com>
2015-11-27 17:40:58 +0000

    Bump patch version

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule DBConnection.Mixfile do
   use Mix.Project
 
   @pools [:connection, :poolboy, :sojourn]
-  @version "0.1.1"
+  @version "0.1.2"
 
   def project do
     [app: :db_connection,
