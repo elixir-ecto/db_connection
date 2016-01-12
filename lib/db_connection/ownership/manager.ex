5c9fde1300ac23d5125fe86d50efc1a7145e6eb8
José Valim <jose.valim@plataformatec.com.br>
2016-01-12 22:12:51 +0100

    Add ETS cache to ownership for registered managers

--- lib/db_connection/ownership/manager.ex
+++ lib/db_connection/ownership/manager.ex
@@ -12,7 +12,7 @@ defmodule DBConnection.Ownership.Manager do
   def start_link(module, opts) do
     pool_mod = Keyword.get(opts, :ownership_pool, DBConnection.Poolboy)
     {owner_opts, pool_opts} = Keyword.split(opts, [:name])
-    GenServer.start_link(__MODULE__, {module, pool_mod, pool_opts}, owner_opts)
+    GenServer.start_link(__MODULE__, {module, owner_opts, pool_mod, pool_opts}, owner_opts)
   end
 
   @spec checkout(GenServer.server, Keyword.t) ::
@@ -46,17 +46,37 @@ defmodule DBConnection.Ownership.Manager do
   @spec lookup(GenServer.server, Keyword.t) ::
     {:ok, pid} | :not_found | :error |
     {kind :: atom, reason :: term, stack :: Exception.stacktrace}
+  def lookup(manager, opts) when is_atom(manager) do
+    client = self()
+    case :ets.lookup(manager, client) do
+      [{^client, owner}] -> {:ok, owner}
+      [] -> server_lookup(manager, opts)
+    end
+  end
+
   def lookup(manager, opts) do
+    server_lookup(manager, opts)
+  end
+
+  defp server_lookup(manager, opts) do
     timeout = Keyword.get(opts, :owner_timeout, @timeout)
     GenServer.call(manager, {:lookup, opts}, timeout)
   end
 
   ## Callbacks
 
-  def init({module, pool_mod, pool_opts}) do
+  def init({module, owner_opts, pool_mod, pool_opts}) do
+    ets =
+      case Keyword.fetch(owner_opts, :name) do
+        {:ok, name} when is_atom(name) ->
+          :ets.new(name, [:named_table, :protected, read_concurrency: true])
+        _ ->
+          nil
+      end
+
     {:ok, pool} = pool_mod.start_link(module, pool_opts)
     mode = Keyword.get(pool_opts, :ownership_mode, :auto)
-    {:ok, %{pool: pool, checkouts: %{}, owners: %{}, mode: mode}}
+    {:ok, %{pool: pool, checkouts: %{}, owners: %{}, mode: mode, ets: ets}}
   end
 
   def handle_call({:mode, mode}, _from, state) do
@@ -89,7 +109,7 @@ defmodule DBConnection.Ownership.Manager do
     end
   end
 
-  def handle_call({:allow, caller, allow}, _from, %{checkouts: checkouts} = state) do
+  def handle_call({:allow, caller, allow}, _from, %{checkouts: checkouts, ets: ets} = state) do
     if kind = already_checked_out(checkouts, allow) do
       {:reply, {:already, kind}, state}
     else
@@ -99,6 +119,7 @@ defmodule DBConnection.Ownership.Manager do
           state = update_in(state.owners[ref], fn {owner, caller, allowed} ->
             {owner, caller, [allow|List.delete(allowed, allow)]}
           end)
+          ets && :ets.insert(ets, {allow, owner})
           {:reply, :ok, state}
         {:allowed, _} ->
           {:reply, :not_owner, state}
@@ -132,21 +153,23 @@ defmodule DBConnection.Ownership.Manager do
     end
   end
 
-  # TODO: Cache using ETS
   defp checkout({caller, _} = from, opts, state) do
-    %{pool: pool, checkouts: checkouts, owners: owners} = state
+    %{pool: pool, checkouts: checkouts, owners: owners, ets: ets} = state
     {:ok, owner} = Supervisor.start_owner(from, pool, opts)
     ref = Process.monitor(owner)
     checkouts = Map.put(checkouts, caller, {:owner, ref, owner})
     owners = Map.put(owners, ref, {owner, caller, []})
+    ets && :ets.insert(ets, {caller, owner})
     %{state | checkouts: checkouts, owners: owners}
   end
 
-  defp owner_down(ref, state) do
+  defp owner_down(ref, %{ets: ets} = state) do
     case get_and_update_in(state.owners, &Map.pop(&1, ref)) do
       {{_owner, caller, allowed}, state} ->
         Process.demonitor(ref, [:flush])
-        update_in(state.checkouts, &Map.drop(&1, [caller|allowed]))
+        entries = [caller|allowed]
+        ets && Enum.each(entries, &:ets.delete(ets, &1))
+        update_in(state.checkouts, &Map.drop(&1, entries))
       {nil, state} ->
         state
     end
