25c068c6975b53eed86370caf0e37543e1a0621d
James Fish <james@fishcakez.com>
2016-06-11 13:41:49 +0100

    Use sbroker package

--- mix.exs
+++ mix.exs
@@ -29,7 +29,7 @@ defmodule DBConnection.Mixfile do
   defp deps do
     [{:connection, "~> 1.0.2"},
      {:poolboy, "~> 1.5", [optional: true]},
-     {:sbroker, "~> 1.0.0-beta.2", [github: "fishcakez/sbroker", tag: "v1.0.0-beta.2", optional: true]},
+     {:sbroker, "~> 1.0.0-beta.2", [optional: true]},
      {:earmark, "~> 0.1", only: :dev},
      {:ex_doc, "~> 0.11.1", only: :dev}]
   end
