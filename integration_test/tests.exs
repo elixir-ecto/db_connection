244db4f280e1be5d0da7a2d73cdfef1ce855028a
Tetiana <tanechka289@gmail.com>
2018-01-13 23:39:24 +0000

    Raise RuntimeError when crashing on connect (#111)
    Remove sensitive data (such as database credentials) when crashing on connect.

--- integration_test/tests.exs
+++ integration_test/tests.exs
@@ -1,5 +1,5 @@
 Code.require_file "cases/after_connect_test.exs", __DIR__
-Code.require_file "cases/backoff_test.exs", __DIR__
+Code.require_file "cases/connect_test.exs", __DIR__
 Code.require_file "cases/client_test.exs", __DIR__
 Code.require_file "cases/close_test.exs", __DIR__
 Code.require_file "cases/execute_test.exs", __DIR__
