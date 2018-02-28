9066ee54273f09b8b06abca035f4357b73c5785d
Mohd Maqbool Alam <maqbool@maqbool.net>
2018-02-28 16:47:19 +0530

    Minor upgrade to /examples

--- examples/tcp_connection/mix.exs
+++ examples/tcp_connection/mix.exs
@@ -7,7 +7,7 @@ defmodule TcpConnection.Mixfile do
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps()]
   end
 
   def application do
