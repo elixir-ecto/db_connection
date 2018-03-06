3235131643070b9247eb09385a6baf72f811bc8f
Mohd Maqbool Alam <maqbool@maqbool.net>
2018-03-06 17:28:47 +0530

    Bump elixir version 1.4

--- examples/db_agent/mix.exs
+++ examples/db_agent/mix.exs
@@ -4,7 +4,7 @@ defmodule DbAgent.Mixfile do
   def project do
     [app: :db_agent,
      version: "0.0.1",
-     elixir: "~> 1.0",
+     elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
--- examples/tcp_connection/mix.exs
+++ examples/tcp_connection/mix.exs
@@ -4,7 +4,7 @@ defmodule TcpConnection.Mixfile do
   def project do
     [app: :tcp_connection,
      version: "0.0.1",
-     elixir: "~> 1.1",
+     elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
