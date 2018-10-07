db5c5706af42f3e8039525b167e74c58df54b460
José Valim <jose.valim@plataformatec.com.br>
2018-10-07 21:45:28 +0200

    Release v2.0.0-rc.0

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule DBConnection.Mixfile do
   use Mix.Project
 
   @pools [:connection_pool, :ownership]
-  @version "2.0.0-dev"
+  @version "2.0.0-rc.0"
 
   def project do
     [app: :db_connection,
