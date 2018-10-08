488d0da7b2117db709defcf2f307980ab3426da6
José Valim <jose.valim@plataformatec.com.br>
2018-10-08 12:13:08 +0200

    Fix handle_execute in DBAgent example

--- examples/db_agent/lib/db_agent.ex
+++ examples/db_agent/lib/db_agent.ex
@@ -5,40 +5,36 @@ defmodule DBAgent do
     defstruct [:query]
   end
 
-  @spec start_link((() -> state :: any), Keyword.t) :: GenServer.on_start
+  @spec start_link((() -> state :: any), Keyword.t()) :: GenServer.on_start()
   def start_link(fun, opts \\ []) when is_function(fun, 0) do
     opts = [init: fun, backoff: nil] ++ opts
     DBConnection.start_link(__MODULE__, opts)
   end
 
-  @spec get(DBConnection.conn, ((state :: any) -> value), timeout) ::
-    value when value: var
+  @spec get(DBConnection.conn(), (state :: any -> value), timeout) :: value when value: var
   def get(conn, fun, timeout \\ 5_000) do
-    DBConnection.execute!(conn, %Query{query: :get}, fun,
-      [pool_timeout: timeout])
+    DBConnection.execute!(conn, %Query{query: :get}, fun, pool_timeout: timeout)
   end
 
-  @spec update(DBConnection.conn, ((state :: any) -> new_state :: any), timeout) ::
-    :ok
+  @spec update(DBConnection.conn(), (state :: any -> new_state :: any), timeout) :: :ok
   def update(conn, fun, timeout \\ 5_000) do
-    DBConnection.execute!(conn, %Query{query: :update}, fun,
-      [pool_timeout: timeout])
+    DBConnection.execute!(conn, %Query{query: :update}, fun, pool_timeout: timeout)
   end
 
-  @spec get(DBConnection.conn, ((state :: any) -> {value, new_state :: any}), timeout) ::
-    value when value: var
+  @spec get(DBConnection.conn(), (state :: any -> {value, new_state :: any}), timeout) :: value
+        when value: var
   def get_and_update(conn, fun, timeout \\ 5_000) do
-    DBConnection.execute!(conn, %Query{query: :get_and_update}, fun,
-      [pool_timeout: timeout])
+    DBConnection.execute!(conn, %Query{query: :get_and_update}, fun, pool_timeout: timeout)
   end
 
-  @spec transaction(DBConnection.conn, ((DBConnection.t) -> res),  timeout) ::
-    {:ok, res} | {:error, reason :: any} when res: var
+  @spec transaction(DBConnection.conn(), (DBConnection.t() -> res), timeout) ::
+          {:ok, res} | {:error, reason :: any}
+        when res: var
   def transaction(conn, fun, timeout \\ 5_000) when is_function(fun, 1) do
-    DBConnection.transaction(conn, fun, [pool_timeout: timeout])
+    DBConnection.transaction(conn, fun, pool_timeout: timeout)
   end
 
-  @spec rollback(DBConnection.t, reason :: any) :: no_return
+  @spec rollback(DBConnection.t(), reason :: any) :: no_return
   defdelegate rollback(conn, reason), to: DBConnection
 
   ## DBConnection API
@@ -56,16 +52,21 @@ defmodule DBAgent do
   def checkin(s), do: {:ok, s}
 
   @impl true
-  def handle_execute(%Query{query: :get}, fun, _, %{state: state} = s) do
-    {:ok, fun.(state), s}
+  def ping(s), do: {:ok, s}
+
+  @impl true
+  def handle_execute(%Query{query: :get} = query, fun, _, %{state: state} = s) do
+    {:ok, query, fun.(state), s}
   end
-  def handle_execute(%Query{query: :update}, fun, _, %{state: state} = s) do
-    {:ok, :ok, %{s | state: fun.(state)}}
+
+  def handle_execute(%Query{query: :update} = query, fun, _, %{state: state} = s) do
+    {:ok, query, :ok, %{s | state: fun.(state)}}
   end
-  def handle_execute(%Query{query: :get_and_update}, fun, _, s) do
+
+  def handle_execute(%Query{query: :get_and_update} = query, fun, _, s) do
     %{state: state} = s
     {res, state} = fun.(state)
-    {:ok, res, %{s | state: state}}
+    {:ok, query, res, %{s | state: state}}
   end
 
   @impl true
@@ -100,8 +101,9 @@ defimpl DBConnection.Query, for: DBAgent.Query do
   def describe(query, _), do: query
 
   def encode(_, fun, _) when is_function(fun, 1), do: fun
+
   def encode(_, other, _) do
-    raise ArgumentError, "#{inspect other} is not 1-arity fun"
+    raise ArgumentError, "#{inspect(other)} is not 1-arity fun"
   end
 
   def decode(_, result, _), do: result
