171211bf814f6a2738d45338ecce942da8ced852
José Valim <jose.valim@dashbit.co>
2024-09-29 21:58:50 +0200

    Improve test reliability

--- test/db_connection_test.exs
+++ test/db_connection_test.exs
@@ -63,21 +63,23 @@ defmodule DBConnectionTest do
   end
 
   describe "connection_module/1" do
-    test "returns the connection module when given a pool pid" do
-      {:ok, pool} = P.start_link([])
+    setup do
+      {:ok, agent} = A.start_link([{:ok, :state}, {:idle, :state}, {:idle, :state}])
+      [agent: agent]
+    end
+
+    test "returns the connection module when given a pool pid", %{agent: agent} do
+      {:ok, pool} = P.start_link(agent: agent)
       assert {:ok, TestConnection} = DBConnection.connection_module(pool)
     end
 
-    test "returns the connection module when given a pool name", %{test: name} do
-      {:ok, _pool} = P.start_link(name: name)
+    test "returns the connection module when given a pool name", %{test: name, agent: agent} do
+      {:ok, _pool} = P.start_link(name: name, agent: agent)
       assert {:ok, TestConnection} = DBConnection.connection_module(name)
     end
 
-    test "returns the connection module when given a locked connection reference" do
-      {:ok, agent} = A.start_link([{:ok, :state}, {:idle, :state}, {:idle, :state}])
-
-      opts = [agent: agent]
-      {:ok, pool} = P.start_link(opts)
+    test "returns the connection module when given a locked connection reference", %{agent: agent} do
+      {:ok, pool} = P.start_link(agent: agent)
 
       P.run(pool, fn conn ->
         assert {:ok, TestConnection} = DBConnection.connection_module(conn)
