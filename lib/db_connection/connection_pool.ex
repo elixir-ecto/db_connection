48bc34d35b557a3f59c7a96a94a3d80f8b04110e
Nathan Long <him@nathanmlong.com>
2025-02-12 10:22:02 -0500

    Document disconnect_all/3 (#322)

--- lib/db_connection/connection_pool.ex
+++ lib/db_connection/connection_pool.ex
@@ -6,6 +6,11 @@ defmodule DBConnection.ConnectionPool do
 
   You're not supposed to call any functions on this pool directly, but only pass this
   as the value of the `:pool` option in functions such as `DBConnection.start_link/2`.
+
+  `disconnect_all/3`, which by default will result in connections being
+  reestablished, can be called periodically to recycle checked-in connections
+  after a maximum lifetime is reached. `Ecto SQL` users may find it at
+  https://hexdocs.pm/ecto_sql/Ecto.Adapters.SQL.html#disconnect_all/3
   """
 
   use GenServer
