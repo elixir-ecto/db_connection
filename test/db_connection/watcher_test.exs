de36728bf2fa6fb3d6adb58c3fe7059217df9fe5
felipe stival <v0idpwn@gmail.com>
2026-04-20 09:13:15 -0300

    Make test concurrency safe

--- test/db_connection/watcher_test.exs
+++ test/db_connection/watcher_test.exs
@@ -5,8 +5,6 @@ defmodule DBConnection.WatcherTest do
   alias TestAgent, as: A
 
   test "starting a new pool is not blocked by a slow-to-terminate pool" do
-    # Start a pool whose connection takes a long time to disconnect.
-    # The disconnect callback sleeps for 10 seconds, simulating a slow shutdown.
     {:ok, agent1} =
       A.start_link([
         {:ok, :state},
@@ -16,20 +14,39 @@ defmodule DBConnection.WatcherTest do
         end
       ])
 
-    start_supervised!(
-      %{
+    pool1 =
+      start_supervised!(%{
         id: :pool1,
-        start: {DBConnection, :start_link, [TestConnection, [agent: agent1, pool_size: 1]]},
+        start:
+          {DBConnection, :start_link,
+           [
+             TestConnection,
+             [agent: agent1, pool_size: 1, idle_interval: 100_000, shutdown: 2_500]
+           ]},
         restart: :temporary
-      }
-    )
+      })
+
+    # Find the pool sup for pool1 by matching the owner pid in child spec ids.
+    pool_sup_ref =
+      DBConnection.ConnectionPool.Supervisor
+      |> DynamicSupervisor.which_children()
+      |> Enum.find_value(fn {_, sup_pid, _, _} ->
+        try do
+          has_pool1? =
+            sup_pid
+            |> Supervisor.which_children()
+            |> Enum.any?(fn {{_mod, owner, _id}, _, _, _} -> owner == pool1 end)
+
+          if has_pool1?, do: sup_pid
+        catch
+          :exit, _ -> nil
+        end
+      end)
+      |> Process.monitor()
 
     # Trigger termination of pool1 by stopping it (the caller).
     # This sends a :DOWN to the Watcher, which will try to terminate the pool.
     stop_supervised!(:pool1)
-
-    # Give the Watcher a moment to receive the :DOWN and start terminating pool1
-    Process.sleep(100)
 
     # Now try to start a second pool. If the Watcher is blocked by the
     # synchronous DynamicSupervisor.terminate_child call, this will hang.
@@ -37,9 +54,12 @@ defmodule DBConnection.WatcherTest do
 
     task =
       Task.async(fn ->
-        C.start_link(agent: agent2, pool_size: 1)
+        C.start_link(agent: agent2, pool_size: 1, idle_interval: 100_000)
       end)
 
     assert {:ok, _pool2} = Task.await(task, 3_000)
+
+    # Ensure the pool supervisor actually terminates
+    assert_receive {:DOWN, ^pool_sup_ref, _, _, _}, 10_000
   end
 end
