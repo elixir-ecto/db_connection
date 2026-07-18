953219f8176f0a1247fac5661621a57bba07fa67
Lukasz Samson <lukaszsamson@gmail.com>
2026-07-18 09:47:57 +0200

    Retry status after disconnect

--- lib/db_connection.ex
+++ lib/db_connection.ex
@@ -1924,6 +1924,7 @@ defmodule DBConnection do
 
       other ->
         case retry_or_handle_common_result(other, conn, meter) do
+          {:retry, _, _} = retry -> retry
           {:error, _, meter} -> {:ok, :error, meter}
           {kind, reason, stack, _meter} -> :erlang.raise(kind, reason, stack)
           _ -> other
